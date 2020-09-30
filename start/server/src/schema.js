5abec7160cd6fa9fcb9eee07ae6e2b758ebe27ee
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-09-30 22:36:46 +0200

    query resolvers done

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,50 +2,64 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
 type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
+    id: ID!
+        site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
 }
 type Rocket {
-  id: ID!
-  name: String
-  type: String
+    id: ID!
+        name: String
+    type: String
 }
 
 type User {
-  id: ID!
-  email: String!
-  trips: [Launch]!
+    id: ID!
+        email: String!
+        trips: [Launch]!
 }
 
 type Mission {
-  name: String
-  missionPatch(size: PatchSize): String
+    name: String
+    missionPatch(size: PatchSize): String
 }
 
 enum PatchSize {
-  SMALL
-  LARGE
+    SMALL
+    LARGE
 }
 
 type Query {
-  launches: [Launch]!
-  launch(id: ID!): Launch
-  me: User
+    launches(
+        """ 
+        The number of results to show. Must be >=1. Default = 20
+        """
+        pageSize: Int
+        """
+        If you add a cursor here, it will only return results _after_ this cursor
+        """
+        after: String
+    ) : LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
 }
 
+type LaunchConnection {
+    cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
+}
 type Mutation {
-  bookTrips(launchIds: [ID]!): TripUpdateResponse!
-  cancelTrip(launchId: ID!): TripUpdateResponse!
-  login(email: String): String # login token
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): String # login token
 }
 
 type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
+    success: Boolean!
+        message: String
+    launches: [Launch]
 }
 
 
