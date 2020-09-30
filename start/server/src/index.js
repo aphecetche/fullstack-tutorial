5bc6a43bdd5dc6c575109f4ef70b03539891f360
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-09-30 18:35:24 +0200

    before pagination

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,13 +1,15 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils')
-
+const resolvers = require('./resolvers')
 const LaunchAPI = require('./datasources/launch')
 const UserAPI = require('./datasources/user')
 
 const store = createStore()
 
-const server = new ApolloServer({ typeDefs ,
+const server = new ApolloServer({ 
+    typeDefs ,
+    resolvers,
     dataSources: () => (
         {
             launchAPI: new LaunchAPI(),
