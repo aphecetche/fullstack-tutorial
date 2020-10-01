045889257caf22af47a8e7704dee73451cd1f2c6
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-10-01 17:13:54 +0200

    6

---
+++ start/client/src/index.jsx
@@ -0,0 +1,20 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import injectStyles from "./styles";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache()
+});
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
+
